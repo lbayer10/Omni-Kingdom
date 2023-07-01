68f68899cc0d948841fb8c6282cf325353f44f84
Pegasus2002 <pegasus.c2002@gmail.com>
2023-07-01 21:28:30 +0800

    update: craft-page

--- src/app/crafts/page.tsx
+++ src/app/crafts/page.tsx
@@ -1,9 +1,32 @@
 import React from "react";
+import { InventoryContainer } from "@/containers/inventory";
+
+import "./style.css";
 
 export default () => {
     return (
         <div>
-            
+            <div className="section h-screen over-hide">
+                <div className="parallax" style={{ backgroundImage: 'url(assets/img/crafts-bg.jpg)', backgroundPosition: '50% -50px', filter: 'grayscale(.5)' }}></div>
+                <div className="grey-fade-over"></div>
+                <div className="crafts-content container">
+                    <div className="row justify-center">
+                        <div className="col-md-7">
+                            <div className="crafts-hero-panel">
+                                <div className="crafts-hero-title"></div>
+                            </div>
+                        </div>
+                        <div className="col-md-5">
+                            <div className="crafts-item-panel">
+                                <div className="crafts-item-title"></div>
+                                <div className="crafts-item-content">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/containers/inventory/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const InventoryContainer = () => {
+    return (
+        <div className="inventory-container">
+
+        </div>
+    )
+}
