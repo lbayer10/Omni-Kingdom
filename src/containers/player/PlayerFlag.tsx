db65e0349660d2592b43a5a9293e51d8eae61688
Pegasus2002 <pegasus.c2002@gmail.com>
2023-07-03 00:11:38 +0800

    update: craft page

---
+++ src/containers/player/PlayerFlag.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Image from "next/image";
+
+export const PlayerFlag = () => {
+    return (
+        <div className='player-drawer-head mx-auto pt-3'>
+            <Image width={90} height={90} className='player-hero-drawer-image mx-auto' src={require('../../assets/img/hero.png')} alt='hero' />
+            <div className='text-center mt-[80px]'>Tyche</div>
+        </div>
+    )
+}
--- src/containers/player/index.tsx
+++ src/containers/player/index.tsx
@@ -1,11 +1,10 @@
 'use client'
 
 import React, { useState, useRef, useEffect } from 'react';
-import Image from 'next/image';
 import { Progress } from '@/components/progress';
-import { PlayerStates } from '@/containers/player/states';
-import LeftOutlined  from '@ant-design/icons/LeftOutlined';
-import RightOutlined from '@ant-design/icons/RightOutlined';
+import { PlayerHand } from '@/containers/player/PlayerHand';
+import { PlayerFlag } from '@/containers/player/PlayerFlag';
+import { PlayerStates } from '@/containers/player/PlayerStates';
 
 import './style.css';
 
@@ -39,26 +38,9 @@ export const PlayerCard = ({
 
     return (
         <div>
-            <div className='player-drawer-shorthand'>
-                <Image className='player-hero-image' width={75} height={70} src={require('../../assets/img/hero.png')} alt='hero' />
-                <div className='flex justify-center'>
-                    <input className='hidden' type="checkbox" id="player-drawer-name" name="player-drawer-name" checked={isShow} onChange={e => setIsShow(e.target.checked)}/>
-                    <label htmlFor="player-drawer-name" className='mt-[50px] text-center cursor-pointer player-drawer-name'>Lion</label>
-                </div>
-            </div>
+            <PlayerHand isShow={isShow} setIsShow={setIsShow} />
             <div className='player-drawer' style={{display: isShow ? 'block' : 'none'}} ref={drawerRef}>
-                <div className='player-drawer-head mx-auto pt-3 flex justify-between'>
-                    <div>
-                        <LeftOutlined />
-                    </div>
-                    <div>
-                        <Image width={90} height={90} className='player-hero-drawer-image mx-auto' src={require('../../assets/img/hero.png')} alt='hero' />
-                        <div className='text-center mt-[80px]'>Tyche</div>
-                    </div>
-                    <div>
-                        <RightOutlined />
-                    </div>
-                </div>
+                <PlayerFlag />
                 <div className='mb-4 mx-4'>
                     <div className='flex'>
                         <div className='player-drawer-level flex items-center justify-center'>Level</div>
