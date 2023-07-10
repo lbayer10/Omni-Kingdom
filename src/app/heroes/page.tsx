a4ae4eb41b18cbc21d1289139cd1e466c65eb1ab
lbayer10 <pegasus.c2002@gmail.com>
2023-07-10 20:58:34 +0800

    update: leaderboard

--- src/app/heroes/page.tsx
+++ src/app/heroes/page.tsx
@@ -9,7 +9,7 @@ export default () => {
         <div className="heroes-container">
             <div className="section h-screen">
                 <div className="parallax" style={{ backgroundImage: 'url(assets/img/heroes-bg.jpg)', backgroundPosition: '50% -50px', filter: 'grayscale(.5)' }}></div>
-                <div className="grey-fade-over"></div>
+                <div className="grey-fade-over grey-fade-over-more"></div>
                 <div className="pt-[130px]">
                     <Container className="" title="Heroes">
                         <HeroesContainer />
--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,9 +1,27 @@
+import { Container } from "@/components/container";
+import { Table } from '@/components/table';
+import { rankingColumn, rankingDataSource } from "@/@fake_db/leaderboard";
+
+export interface ColumnType {
+    key: string;
+    title: string;
+    dataIndex: string;
+    width?: number;
+    align?: "left" | "center" | "right";
+    render?: React.ReactElement;
+}
+
 export default () => {
     return (
         <div className="leaderboard-container">
             <div className="section h-screen over-hide">
                 <div className="parallax" style={{ backgroundImage: 'url(assets/img/leaderboard-bg.jpg)', backgroundPosition: '50% -50px', filter: 'grayscale(.5)' }}></div>
-                <div className="grey-fade-over"></div>
+                <div className="grey-fade-over grey-fade-over-more"></div>
+                <div className="pt-[130px]">
+                    <Container title="Leaderboard">
+                        <Table column={rankingColumn} dataSource={rankingDataSource} />
+                    </Container>
+                </div>
             </div>
         </div>
     )
